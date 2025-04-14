4721c705cc191da29b5007d5782fc133b8150a39
SUBHAJIT555 <subhajitdhali0@gmail.com>
2025-04-14 13:26:01 +0400

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,22 +9,12 @@ const Hero = () => {
       {/* Background Video */}
       <video
         className="absolute top-0 left-0 h-full w-full object-cover z-10"
+        src={herovideo}
         autoPlay
         loop
         muted
         playsInline
-        poster={herocontent}
-      >
-        <source
-          src="../assets/images/homeImages/heroVideo.webm"
-          type="video/webm"
-        />
-        <source
-          src="../assets/images/homeImages/heroVideo.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+      />
 
       <div className="absolute top-[30%] left-[10%] z-20 h-[125px] w-[230px] md:h-[170px] md:w-[313px] lg:h-[224px] lg:w-[500px]">
         <img src={herocontent} alt="" />
